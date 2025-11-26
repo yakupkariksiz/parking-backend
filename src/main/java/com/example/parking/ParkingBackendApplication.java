1053fdf290d04fb89b04d11776feed6707994f32
yakup.kariksiz@gmail.com <yakupkariksiz@gmail.com>
2025-11-26 01:27:57 +0100

    all dates in Amsterdam timezone.

--- src/main/java/com/example/parking/ParkingBackendApplication.java
+++ src/main/java/com/example/parking/ParkingBackendApplication.java
@@ -1,12 +1,21 @@
 package com.example.parking;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class ParkingBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ParkingBackendApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        // Tüm uygulama için default timezone
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Amsterdam"));
     }
 }
